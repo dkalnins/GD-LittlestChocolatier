f051c7cb02d8ffedda1a0abe8fa2d72eb3e52e90
David Kalnins <dkalnins@gmail.com>
2023-08-12 17:48:36 +1000

    added jumping and ground checks

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Player Physics")]
+    [SerializeField] private float _walkSpeed = 7f;
+    [SerializeField] private float _jumpVelocity = 12f;
 
-    private Rigidbody2D _rb;
+    private Rigidbody2D _rigidBody;
     private Animator _animator;
+    private BoxCollider2D _collider;
+
+    [SerializeField]private LayerMask _jumpableLayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();
+        BindComponentVariables();
+    }
+
+    private void BindComponentVariables()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        Assert.IsNotNull(_rigidBody);
+
         _animator = GetComponent<Animator>();
+        Assert.IsNotNull(_animator);
+
+        _collider = GetComponent<BoxCollider2D>();
+        Assert.IsNotNull(_collider);
+    }
+
+
+    /// <summary>
+    /// Works out whether the player is "grounded", which can affect player behaviour and
+    /// animations.
+    /// 
+    /// Grounded check is based off the the collider attached to the player.
+    /// </summary>
+    bool IsGrounded()
+    {
+        return Physics2D.BoxCast(_collider.bounds.center, _collider.bounds.size, 0, Vector2.down, .1f, _jumpableLayer);
     }
 
     // Update is called once per frame
@@ -22,11 +55,22 @@ public class PlayerMovement : MonoBehaviour
     {
         float xDirection = Input.GetAxisRaw("Horizontal");
 
-        _rb.velocity = new Vector2(xDirection * 7f, _rb.velocity.y);
+        _rigidBody.velocity = new Vector2(xDirection * _walkSpeed, _rigidBody.velocity.y);
 
+        if (IsGrounded())
+        {
+            HandleMovementControls(xDirection);
+        }
+
+
+
+    }
+
+    private void HandleMovementControls(float xDirection)
+    {
         if (Input.GetButton("Jump"))
         {
-            _rb.velocity = new Vector2(_rb.velocity.x, 7f);
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _jumpVelocity);
         }
 
         if (xDirection >= 0.001f)
@@ -41,7 +85,5 @@ public class PlayerMovement : MonoBehaviour
         {
             _animator.SetBool("isWalking", false);
         }
-
-
     }
 }
